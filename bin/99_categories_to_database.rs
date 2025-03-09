bd1d1847a3d04d7c46eec46121c186e7e0a00d8e
howlrs <sharebook.amazon@gmail.com>
2025-03-10 08:31:40 +0900

    set db

---
+++ bin/99_categories_to_database.rs
@@ -0,0 +1,57 @@
+use std::env;
+
+use log::{error, info};
+
+mod utils;
+
+#[tokio::main]
+async fn main() {
+    dotenv::dotenv().ok();
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
+
+    let output_dir = "output";
+    let target_dir = "questions";
+    let target_filename = "5_categories_meta.json";
+
+    // データベース登録関数はTであるため、読み込む型に応じた変数を用意する
+    let mut values = Vec::<crate::utils::CatValue>::new();
+
+    // 出力の可否
+    let is_output = true;
+    let collection_name = "categories_meta";
+
+    // 対象ディレクトリを指定し、ファイルを読み込む
+    let target_level_dir = {
+        let current_dir = env::current_dir().unwrap();
+        current_dir.join(output_dir).join(target_dir)
+    };
+
+    let target_filepath = target_level_dir.join(target_filename);
+    if !target_filepath.exists() {
+        error!("ファイルが存在しません: {:?}", target_filepath);
+        return;
+    }
+
+    let content = crate::utils::read_file(target_filepath.clone());
+    values = match serde_json::from_str(&content) {
+        Ok(values) => values,
+        Err(e) => {
+            error!("ファイルが存在しません: {:?}, {:?}", target_filepath, e);
+            return;
+        }
+    };
+
+    let err_values = crate::utils::to_database(is_output, collection_name, values).await;
+
+    if !err_values.is_empty() {
+        info!(
+            "エラーが発生しました、保存できなかったQをerr.jsonに保存します: {:?}",
+            err_values.len()
+        );
+        let to_json = serde_json::to_string_pretty(&err_values).unwrap();
+        let output_filepath = target_level_dir.join("save_err_to_db.json");
+        crate::utils::write_file(output_filepath, to_json.as_str());
+    }
+
+    info!("done");
+}
--- bin/utils.rs
+++ bin/utils.rs
@@ -1,12 +1,121 @@
-use std::{
-    env, fs,
-    path::{Path, PathBuf},
-};
-
+use firestore::FirestoreDb;
 use google_generative_ai_rs::v1::{
     api::Client,
     gemini::{Content, Model, Part, Role, request::Request},
 };
+use log::{error, info};
+use serde::{Deserialize, Serialize};
+use std::{
+    collections::HashMap,
+    env, fs,
+    path::{Path, PathBuf},
+};
+use uuid::Uuid;
+
+pub trait DBInsertTrait {
+    #[allow(unused)]
+    fn id(&self) -> String;
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, Default)]
+pub struct Question {
+    #[serde(default)]
+    pub id: Option<String>,
+    #[serde(default)]
+    pub level_id: u32,
+    pub level_name: String,
+    #[serde(default)]
+    pub category_id: u32,
+    pub category_name: String,
+
+    #[serde(default)]
+    pub chapter: String,
+    pub sentence: String,
+    pub prerequisites: Option<String>,
+    pub sub_questions: Vec<SubQuestion>,
+}
+
+impl DBInsertTrait for Question {
+    fn id(&self) -> String {
+        self.id
+            .clone()
+            .unwrap_or(
+                Uuid::new_v4()
+                    .to_string()
+                    .chars()
+                    .filter(|c| c.is_alphanumeric())
+                    .collect::<String>(),
+            )
+            .clone()
+    }
+}
+
+impl Question {
+    #[allow(unused)]
+    pub fn numbering(&mut self) {
+        self.id = Some(Uuid::new_v4().to_string());
+
+        let mut child_id = 1;
+        for sub_question in self.sub_questions.iter_mut() {
+            sub_question.id = child_id;
+            child_id += 1;
+        }
+    }
+
+    // レベルを数値に変換
+    // 前提: level_nameがある
+    // level_name: n1, n2, n3, n4, n5
+    #[allow(unused)]
+    pub fn leveling(&mut self) {
+        let level = self.level_name.clone();
+        // n3, n2, n1のnを取り除き、数字のみを取得
+        let level_number = level.chars().skip(1).collect::<String>();
+        if level_number.is_empty() {
+            error!("level_number is empty");
+            return;
+        }
+
+        match level_number.parse::<u32>() {
+            Ok(n) => self.level_id = n,
+            Err(e) => {
+                error!("level_number parse error: {}", e);
+            }
+        };
+    }
+}
+
+type SelectAnswer = HashMap<String, String>;
+
+#[derive(Serialize, Deserialize, Debug, Clone, Default)]
+pub struct SubQuestion {
+    #[serde(default)]
+    pub id: u32,
+    #[serde(default)]
+    pub hint_id: u32,
+    #[serde(default)]
+    pub answer_id: u32,
+
+    pub sentence: Option<String>,
+    pub prerequisites: Option<String>,
+    pub select_answer: Vec<SelectAnswer>,
+    pub answer: String,
+
+    #[serde(default)]
+    pub voted: Option<i32>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct CatValue {
+    pub level_id: u32,
+    pub id: u32,
+    pub name: String,
+}
+
+impl DBInsertTrait for CatValue {
+    fn id(&self) -> String {
+        self.id.to_string()
+    }
+}
 
 #[allow(unused)]
 // 指定ディレクトリのファイルを走査する
@@ -42,11 +151,13 @@ pub fn replace_target(target: &str, line: &str) -> String {
     line.replace(target, "")
 }
 
+#[allow(unused)]
 pub fn replace_level(text: &str, target_level: &str) -> String {
     let replace_text = target_level.to_string().to_uppercase();
     text.replace("**LEVEL**", &format!("**{}**", replace_text))
 }
 
+#[allow(unused)]
 pub fn get_key_and_model() -> (String, String) {
     let key = match env::var("GOOGLE_GEMINI_API_KEY") {
         Ok(k) => k,
@@ -70,6 +181,7 @@ pub fn get_key_and_model() -> (String, String) {
 }
 
 // gemini api request
+#[allow(unused)]
 pub async fn request_gemini_api(key: String, model: String, text: &str) -> Result<String, String> {
     let client = Client::new_from_model(Model::Custom(model.to_string()), key.to_string());
     let request = Request {
@@ -105,4 +217,49 @@ pub async fn request_gemini_api(key: String, model: String, text: &str) -> Resul
         Some(t) => Ok(t.to_string()),
         None => Err("Error".to_string()),
     }
+}
+
+#[allow(unused)]
+pub async fn to_database<T>(is_output: bool, collection_name: &str, values: Vec<T>) -> Vec<T>
+where
+    T: crate::utils::DBInsertTrait + Serialize + for<'de> Deserialize<'de> + Send + Sync,
+{
+    let project_id = std::env::var("PROJECT_ID").expect("PROJECT_ID must be set");
+
+    let firestore = match FirestoreDb::new(project_id).await {
+        Ok(firestore) => firestore,
+        Err(e) => {
+            error!("FirestoreDb::new failed: {:?}", e);
+            return values;
+        }
+    };
+
+    let mut err_values = vec![];
+
+    for (index, value) in values.into_iter().enumerate() {
+        if !is_output {
+            info!("[not save] insert: {} {}", index, value.id());
+            continue;
+        }
+
+        // let uid = Uuid::new_v4().to_string();
+
+        match firestore
+            .fluent()
+            .insert()
+            .into(collection_name)
+            .document_id(value.id())
+            .object(&value)
+            .execute::<T>()
+            .await
+        {
+            Ok(_) => info!("inserted: {} {}", index, value.id()),
+            Err(e) => {
+                error!("insert failed: {} {}, {}", index, value.id(), e);
+                err_values.push(value);
+            }
+        };
+    }
+
+    err_values
 }
